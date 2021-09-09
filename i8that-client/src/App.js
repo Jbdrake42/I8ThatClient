d6f7426b912f62d40bedd03ae5d97d4092f1e3f0
aspenduvall <40346050+aspenduvall@users.noreply.github.com>
2021-09-09 17:46:43 -0400

    Update App.js

--- i8that-client/src/App.js
+++ i8that-client/src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import Header from './Components/Header';
 import Sitebar from './Components/Sitebar';
 import Home from './Home/Home';
-import FoodIndex from "./Food/FoodIndex";
 import Auth from "./Auth/Auth";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
